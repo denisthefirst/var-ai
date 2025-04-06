c1ce4fd377cb027b3ddb896a85e572f1f69d3819
Denis <denis@denis.fritz.box>
2025-04-06 22:34:22 +0200

    adding detect human poses

--- src/modelling/inference.py
+++ src/modelling/inference.py
@@ -1,7 +1,15 @@
-from ultralytics import YOLO
+from ultralytics import YOLO # type: ignore
 
+absolute_path = "/home/denis/Projects/var-ai/"
 
-def detect_object_in_video(video_path, output_path, model_path="../../models/pretrained/yolov8n.pt", save_output=True):
+def detect_object_in_video(video_path, output_path, model_path=f"{absolute_path}models/pretrained/yolo11n.pt", save_output=True):
+    model = YOLO(model_path)
+
+    results = model(video_path, save=save_output, project=output_path)
+
+    return results
+
+def detect_human_poses_in_video(video_path, output_path, model_path=f"{absolute_path}models/pretrained/yolo11n-pose.pt", save_output=True):
     model = YOLO(model_path)
 
     results = model(video_path, save=save_output, project=output_path)
