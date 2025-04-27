1de1fff8f490903bfdc9f5c4a9498e291b844178
Denis <denis@denis.fritz.box>
2025-04-27 15:16:59 +0200

    adding inference executable script for performing object detection on all raw videos

--- src/modelling/inference.py
+++ src/modelling/inference.py
@@ -1,7 +1,10 @@
 import os
 from ultralytics import YOLO  # type: ignore
 
-root_path = os.path.abspath(os.path.join(os.getcwd(), '..'))
+script_dir = os.path.dirname(__file__)
+proj_root_dir = os.path.join(script_dir, '..', '..')
+video_download_path = os.path.join(proj_root_dir, 'data', 'raw')
+object_detected_video = os.path.join(proj_root_dir, 'data', 'object_detected')
 
 """
 Module for running inferences on various models used in this project
@@ -19,8 +22,7 @@ def detect_object_in_video(video_path, output_path, save_output=True):
     Returns:
         results: The results from the YOLO model, typically including detected objects, their locations, and confidence scores.
     """
-    global root_path
-    model_path = f"{root_path}/models/pretrained/yolo11n.pt"
+    model_path = f"{proj_root_dir}/models/pretrained/yolo11n.pt"
     model = YOLO(model_path)
     results = model(video_path, save=save_output, project=output_path)
     return results
@@ -38,8 +40,42 @@ def detect_human_poses_in_video(video_path, output_path, save_output=True):
     Returns:
         results: The results from the YOLO model, typically including the detected poses and keypoints.
     """
-    global root_path
-    model_path = f"{root_path}/models/pretrained/yolo11n-pose.pt"
+    model_path = f"{proj_root_dir}/models/pretrained/yolo11n-pose.pt"
     model = YOLO(model_path)
     results = model(video_path, save=save_output, project=output_path)
     return results
+
+def scan_videos_dir() -> list:
+    """
+    This function returns all filenames inside the download
+    directory.
+
+    :returns: A list with filenames and file extension.
+    """
+    try:
+        return os.listdir(video_download_path)
+    except Exception as e:
+        print(f"Error reading download directory: {e}")
+        return []
+
+
+def run_object_detection_on_all_videos():
+    """
+    Run object detection on all videos in the download directory that haven't been processed yet.
+    
+    Returns:
+        None
+    """
+    downloaded_files = scan_videos_dir() 
+    
+    for file in downloaded_files:
+        video_path = os.path.join(video_download_path, file)
+        print(f"Processing: {file}")
+        detect_object_in_video(video_path, object_detected_video)
+
+def main():
+    run_object_detection_on_all_videos()
+
+
+if __name__ == "__main__":
+    main()
